3d996025bfcb4563ce349a693d23703130698b92
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-06-22 14:58:21 -0400

    firebaseui for auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import { keys } from './keys';
 var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
