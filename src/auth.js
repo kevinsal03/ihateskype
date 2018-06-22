dbd107a489167ecc30ff93283aa39fa60b85b9c7
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-06-22 17:27:00 -0400

    not finished had t ogo somewhere

--- src/auth.js
+++ src/auth.js
@@ -1,2 +1,32 @@
 var firebase = require('firebase');
 var firebaseApp = require('./firebase.js');
+
+//Sign Up Handler
+
+document.getElementById("signUpBtn").addEventListener('click', e=>{
+  const email = document.getElementById("signUpEmailInput").value;
+  const pass = document.getElementById("signUpPasswordInput").value;
+  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+   console.log(error.message);
+  });
+})
+
+//Login Handeler
+document.getElementById("loginBtn").addEventListener('click', e=>{
+  const email = document.getElementById("loginEmailInput").value;
+  const pass = document.getElementById("loginEmailInput").value;
+  const promise = firebase.auth().signInWithEmailAndPassword(email, pass);
+  promise.catch(e=>{ console.log(e.massage)})
+})
+
+//Logout Handler
+firebase.auth().onAuthStateChanged(user=>{
+  if(user) {
+    document.getElementById("logOutBtn").classList.remove('hide')
+    document.getElementById("userControlDiv").classlist.add('hide')
+  } else {
+    document.getElementById("logOutBtn").classList.add('hide')
+    document.getElementById("userControlDiv").classlist.remove('hide')
+
+  }
+})
