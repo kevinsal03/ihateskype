fa2e89ac0b0fd86b12c537b026219d8e275ebfd2
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-06-22 14:31:59 -0400

    fixed a typo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
-import { keys } from 'keys';
+import { keys } from './keys';
+var firebase = require('firebase');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
