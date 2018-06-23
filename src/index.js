57ba9e2ebd426f92228981d3563f777e1dc94540
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-06-23 15:38:09 -0400

    changed default window size to fit home page design better

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ let mainWindow;
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1500,
+    height: 850,
   });
 
   // and load the index.html of the app.
