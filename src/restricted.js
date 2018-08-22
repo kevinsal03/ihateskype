d15f7d68204272e0ad1e7274fe78fd5017987ece
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-08-21 22:19:25 -0400

    login updates and chat send workingISH

--- src/restricted.js
+++ src/restricted.js
@@ -4,4 +4,11 @@ if(store.get('token') == undefined){
   console.log("logged in");
   console.log(store.get('token'));
   console.log(store.get('user'));
+  console.log(store.get('uid'));
+}
+function signOut(){
+  store.delete('token');
+  store.delete('user');
+  store.delete('uid')
+  window.location = window.location;
 }
